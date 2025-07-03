c79dee763db191e5f307cc2a5b1c16f8fcbb6e1a
1F10240198 <s1F102401988@iniad.org>
2025-07-03 10:51:32 +0900

    testを修正

--- todo/tests.py
+++ todo/tests.py
@@ -54,7 +54,7 @@ class TaskModelTestCase(TestCase):
         self.assertFalse(task.is_overdue(current))
 
 class TodoViewTestCase(TestCase):
-    def test_index_ge(self):
+    def test_index_get(self):
         client = Client()
         response = client.get('/')
 
